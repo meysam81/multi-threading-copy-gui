aa39d6d674799543f88ad039fece3fd22aae11ae
Meysam Azad <MeysamAzad81@yahoo.com>
2018-01-16 09:56:38 +0330

    tested and running functionally

--- copy-multithread-gui/MainForm.Designer.cs
+++ copy-multithread-gui/MainForm.Designer.cs
@@ -15,13 +15,13 @@ namespace copy_multithread_gui
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
 		private System.Windows.Forms.Button buttonStart;
-		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBoxSrc;
 		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBoxDest;
 		private System.Windows.Forms.Label label2;
-		private System.Windows.Forms.Label label3;
-		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBoxNThrd;
 		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button buttonExit;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -45,30 +45,32 @@ namespace copy_multithread_gui
 		private void InitializeComponent()
 		{
 			this.buttonStart = new System.Windows.Forms.Button();
-			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBoxSrc = new System.Windows.Forms.TextBox();
 			this.label1 = new System.Windows.Forms.Label();
-			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBoxDest = new System.Windows.Forms.TextBox();
 			this.label2 = new System.Windows.Forms.Label();
-			this.label3 = new System.Windows.Forms.Label();
-			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBoxNThrd = new System.Windows.Forms.TextBox();
 			this.label4 = new System.Windows.Forms.Label();
+			this.buttonExit = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// buttonStart
 			// 
-			this.buttonStart.Location = new System.Drawing.Point(371, 234);
+			this.buttonStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonStart.Location = new System.Drawing.Point(290, 86);
 			this.buttonStart.Name = "buttonStart";
 			this.buttonStart.Size = new System.Drawing.Size(75, 23);
 			this.buttonStart.TabIndex = 3;
 			this.buttonStart.Text = "Start";
 			this.buttonStart.UseVisualStyleBackColor = true;
+			this.buttonStart.Click += new System.EventHandler(this.ButtonStartClick);
 			// 
-			// textBox1
+			// textBoxSrc
 			// 
-			this.textBox1.Location = new System.Drawing.Point(119, 9);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(327, 20);
-			this.textBox1.TabIndex = 0;
+			this.textBoxSrc.Location = new System.Drawing.Point(119, 9);
+			this.textBoxSrc.Name = "textBoxSrc";
+			this.textBoxSrc.Size = new System.Drawing.Size(327, 20);
+			this.textBoxSrc.TabIndex = 0;
 			// 
 			// label1
 			// 
@@ -79,12 +81,12 @@ namespace copy_multithread_gui
 			this.label1.Text = "Source File";
 			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
-			// textBox2
+			// textBoxDest
 			// 
-			this.textBox2.Location = new System.Drawing.Point(119, 35);
-			this.textBox2.Name = "textBox2";
-			this.textBox2.Size = new System.Drawing.Size(327, 20);
-			this.textBox2.TabIndex = 1;
+			this.textBoxDest.Location = new System.Drawing.Point(119, 35);
+			this.textBoxDest.Name = "textBoxDest";
+			this.textBoxDest.Size = new System.Drawing.Size(327, 20);
+			this.textBoxDest.TabIndex = 1;
 			// 
 			// label2
 			// 
@@ -95,21 +97,12 @@ namespace copy_multithread_gui
 			this.label2.Text = "Destination File";
 			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
-			// label3
+			// textBoxNThrd
 			// 
-			this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-			this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.label3.Location = new System.Drawing.Point(12, 89);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(434, 142);
-			this.label3.TabIndex = 2;
-			// 
-			// textBox3
-			// 
-			this.textBox3.Location = new System.Drawing.Point(119, 61);
-			this.textBox3.Name = "textBox3";
-			this.textBox3.Size = new System.Drawing.Size(327, 20);
-			this.textBox3.TabIndex = 2;
+			this.textBoxNThrd.Location = new System.Drawing.Point(119, 61);
+			this.textBoxNThrd.Name = "textBoxNThrd";
+			this.textBoxNThrd.Size = new System.Drawing.Size(327, 20);
+			this.textBoxNThrd.TabIndex = 2;
 			// 
 			// label4
 			// 
@@ -120,22 +113,35 @@ namespace copy_multithread_gui
 			this.label4.Text = "Number of Threads";
 			this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
+			// buttonExit
+			// 
+			this.buttonExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.buttonExit.Location = new System.Drawing.Point(371, 86);
+			this.buttonExit.Name = "buttonExit";
+			this.buttonExit.Size = new System.Drawing.Size(75, 23);
+			this.buttonExit.TabIndex = 4;
+			this.buttonExit.Text = "Exit";
+			this.buttonExit.UseVisualStyleBackColor = true;
+			this.buttonExit.Click += new System.EventHandler(this.ButtonExit);
+			// 
 			// MainForm
 			// 
 			this.AcceptButton = this.buttonStart;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(458, 260);
-			this.Controls.Add(this.label3);
+			this.CancelButton = this.buttonExit;
+			this.ClientSize = new System.Drawing.Size(458, 112);
 			this.Controls.Add(this.label4);
 			this.Controls.Add(this.label2);
-			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBoxNThrd);
 			this.Controls.Add(this.label1);
-			this.Controls.Add(this.textBox2);
-			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.textBoxDest);
+			this.Controls.Add(this.textBoxSrc);
+			this.Controls.Add(this.buttonExit);
 			this.Controls.Add(this.buttonStart);
-			this.MaximumSize = new System.Drawing.Size(474, 299);
-			this.MinimumSize = new System.Drawing.Size(474, 299);
+			this.MaximumSize = new System.Drawing.Size(474, 151);
+			this.MinimumSize = new System.Drawing.Size(474, 151);
 			this.Name = "MainForm";
 			this.ShowIcon = false;
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
--- copy-multithread-gui/MainForm.cs
+++ copy-multithread-gui/MainForm.cs
@@ -4,30 +4,119 @@
  * Date: 1/16/2018
  * Time: 08:31
  * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
-using System.Collections.Generic;
-using System.Drawing;
+using System.Threading;
+using System.IO;
 using System.Windows.Forms;
+using System.Linq;
 
 namespace copy_multithread_gui
 {
-	/// <summary>
-	/// Description of MainForm.
-	/// </summary>
 	public partial class MainForm : Form
 	{
+		
 		public MainForm()
 		{
-			//
-			// The InitializeComponent() call is required for Windows Forms designer support.
-			//
 			InitializeComponent();
+		}
+		
+		void ButtonStartClick(object sender, EventArgs e)
+		{
+			Copy c = null;
+			if (textBoxSrc.Text != "" && textBoxDest.Text != "")
+			{
+				c = new Copy(textBoxSrc.Text, textBoxDest.Text);
+				if (textBoxNThrd.Text != "")
+				{
+					try
+					{
+						int count = Convert.ToInt32(textBoxNThrd.Text);
+						c.startFunc(count); // starts the function
+					}
+					catch (Exception ex)
+					{
+						MessageBox.Show(ex.StackTrace + "\r\n" + ex.Message);
+					}
+				}
+			}
 			
-			//
-			// TODO: Add constructor code after the InitializeComponent() call.
-			//
+		}
+		void ButtonExit(object sender, EventArgs e)
+		{
+			Environment.Exit(0);
+		}
+	}
+	class Copy
+	{
+		// a lock mechanism to avoid race condition i.e. collisions of threads for shared resource
+		static Semaphore readSemaphore = new Semaphore(1, 1),
+		writeSemaphore = new Semaphore(1, 1);
+		
+		static int srcHead = 0; // source file head location
+		string sourceLocation, destLocation; // location of source and destination file
+		public Copy(string srcLoc, string destLoc) // constructor used for initializing threads
+		{
+			this.sourceLocation = srcLoc;
+			this.destLocation = destLoc;
+		}
+		public void startFunc(int numberOfThreads)
+		{
+			try
+			{
+				int N = // number of threads
+					numberOfThreads;
+				Thread[] t = new Thread[N];
+				Copy[] pr = new Copy[N];
+				for (int i = 0; i < N; i++)
+				{
+					pr[i] = new Copy(sourceLocation, destLocation);
+					t[i] = new Thread(new ThreadStart(pr[i].thrdFunc));
+					t[i].Name = (i + 1).ToString();
+				}
+				for (int i = 0; i < N; i++)
+					t[i].Start(); // multithreding starts from here
+				MessageBox.Show("Done");
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.StackTrace + "\r\n" + ex.Message);
+			}
+		}
+		public void thrdFunc()
+		{
+			while (true) {
+				try {
+					
+					string tmp = null;
+					
+					readSemaphore.WaitOne(); // get the lock
+					
+					// read srcHead'th line
+					tmp = File.ReadLines(sourceLocation).Skip(srcHead).FirstOrDefault();
+					
+					// increase srchead for next read from source
+					srcHead += 1;
+					
+					readSemaphore.Release(); // release the lock
+					
+					if (tmp == null) // nothing to read i.e. end of file; so no need to write
+					{
+						break;
+					}
+					
+					writeSemaphore.WaitOne();
+					using (StreamWriter sw = new StreamWriter(destLocation, true))
+					{
+						sw.WriteLine(tmp);
+					}
+					writeSemaphore.Release();
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show(ex.StackTrace + "\r\n" + ex.Message);
+				}
+			}
 		}
 	}
 }
